285a912d1923fe75ac67ca0f5e4e284b194d90df
SimonGreenhill <simon@simon.net.nz>
2022-05-04 11:01:35 +1200

    rebuild

--- cldfbench_kolipakam_et_al2018.py
+++ cldfbench_kolipakam_et_al2018.py
@@ -9,22 +9,17 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
+
         args.writer.add_summary(
             self.raw_dir.read_tree(
                 'rsos171504supp5.trees', detranslate=True),
             self.metadata,
             args.log)
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('drav_cov_est_ucln_yule_no_burnin.trees.gz'),
-                1000),
-            detranslate=True,
-            as_nexus=True)
 
-        args.writer.add_posterior(
-            posterior.trees.trees, 
-            self.metadata, 
-            args.log)
+        posterior = self.raw_dir.read_trees(
+            'drav_cov_est_ucln_yule_no_burnin.trees.gz',
+            burnin=1000, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
 
         args.writer.add_data(
             self.raw_dir.read_nexus('drav.nex'),
